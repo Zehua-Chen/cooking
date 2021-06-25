a2137151f2a20ed4e4fc31d7171cf30c00dd1e40
Zehua-Chen <peterchen06242000@outlook.com>
2021-06-25 09:14:56 -0500

    [refactor] Setup Vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Ass we can</h1>
+    <button v-on:click="fuck">fuck</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    fuck() {
+      console.log("fuck");
+    }
+  }
+};
+</script>
