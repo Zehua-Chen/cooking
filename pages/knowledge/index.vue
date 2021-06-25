8ae8993065eff371a5f5ff7479678558289294d2
Zehua-Chen <peterchen06242000@outlook.com>
2021-06-25 10:47:24 -0500

    [feat] Query Knowledge

---
+++ pages/knowledge/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="article in articles" :key="article.title">
+      <NuxtLink v-bind:to="article.path">{{ article.title }}</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const articles = await $content("knowledge").fetch();
+
+    return {
+      articles
+    };
+  }
+};
+</script>
---
+++ pages/recipes/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const slug = params.slug || "borscht";
+    const page = await $content("recipes/", slug).fetch();
+
+    return {
+      page
+    };
+  }
+};
+</script>
