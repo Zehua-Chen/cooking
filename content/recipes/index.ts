b84f4743401c245a41e4634675ce21d73819ac3d
Zehua Chen <zehua-chen@outlook.com>
2022-02-06 15:52:30 -0500

    Docs and Test (#15)
    * [docs] Document content/recipes/index.ts
    
    * [refactor] Add Typing to TOC

--- content/recipes/index.ts
+++ content/recipes/index.ts
@@ -1,17 +1,62 @@
+import { IContentDocument } from "@nuxt/content/types/content";
+
+/**
+ * Ingredients of a recipe
+ */
 export interface Ingredient {
+  /**
+   * Name of a recipe
+   */
   name: string;
+
+  /**
+   * If this ingredient is optional
+   */
   optional?: boolean;
+
+  /**
+   * Quantity to use with this ingredient
+   */
   quantity?: number;
+
+  /**
+   * Unit to use with the quantity of this ingredient
+   */
   unit?: number;
+
+  /**
+   * If this recipe belongs **only** to a variant of a recipe. This property
+   * contains name of the variant
+   */
   variant?: string;
 }
 
+/**
+ * Defines a variant to a recipe
+ */
 export interface Variant {
+  /**
+   * Name of the recipe
+   */
   name: string;
 }
 
-export interface Page {
+/**
+ * Defines a recipe
+ */
+export interface Recipe extends IContentDocument {
+  /**
+   * Title of a recipe
+   */
+  title: string;
+
+  /**
+   * Variants of this recipe
+   */
   variants: Variant[];
+
+  /**
+   * Ingredients of this variants
+   */
   ingredients?: Ingredient[];
-  toc: any[];
 }
