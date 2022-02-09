c97fdb1527cf3ac9ee43a01d8e4f7004f6467d7c
Zehua-Chen <zehua-chen@outlook.com>
2022-02-09 15:45:47 -0500

    [feat] Setup Tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,23 @@ import path from "path";
 export default {
   alias: {
     components: path.resolve(__dirname, "components"),
-    layouts: path.resolve(__dirname, "layouts")
+    layouts: path.resolve(__dirname, "layouts"),
   },
   target: "static",
-  css: ["assets/css/index.scss"],
+  css: ["assets/css/main.css"],
   buildModules: [
+    "@nuxt/postcss8",
     "@nuxtjs/color-mode",
-    ["@nuxt/typescript-build", { typeCheck: true }]
+    ["@nuxt/typescript-build", { typeCheck: true }],
   ],
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
   modules: ["@nuxt/content"],
-  content: {}
+  content: {},
 };
