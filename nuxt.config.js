16e651cf7778090f037c4a778585ad7df3a5c9f7
Zehua Chen <zehua-chen@outlook.com>
2022-11-19 04:51:01 +0000

    [feat] Enable Localization

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,20 @@
+import { defineNuxtConfig } from "nuxt/config";
 import path from "path";
 
-export default {
+export default defineNuxtConfig({
   alias: {
     components: path.resolve(__dirname, "components"),
     layouts: path.resolve(__dirname, "layouts"),
     models: path.resolve(__dirname, "models"),
   },
-  target: "static",
-  css: ["assets/css/main.css"],
-  buildModules: [
-    "@nuxtjs/color-mode",
-    ["@nuxt/typescript-build", { typeCheck: true }],
-  ],
-  build: {
-    // extract css during development would cause the browser to cache the
-    // style sheet. Changes to stylesheet would therefore not be reflected
-    // correctly
-    extractCSS: process.env.NODE_ENV === "production",
-  },
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
-  content: {},
-  colorMode: {
-    classSuffix: "",
-  },
+  modules: ["@nuxt/content", "@nuxtjs/i18n"],
   i18n: {
     locales: ["zh_CN"],
     defaultLocale: "zh_CN",
     vueI18n: {
+      legacy: false,
+      locale: "zh_CN",
       messages: {
-        fallbackLocale: "zh_CN",
         zh_CN: {
           breakfast: "早饭",
           lunch: "午饭",
@@ -39,4 +25,24 @@ export default {
       },
     },
   },
-};
+});
+
+// export default {
+//   target: "static",
+//   css: ["assets/css/main.css"],
+//   buildModules: [
+//     "@nuxtjs/color-mode",
+//     ["@nuxt/typescript-build", { typeCheck: true }],
+//   ],
+//   build: {
+//     // extract css during development would cause the browser to cache the
+//     // style sheet. Changes to stylesheet would therefore not be reflected
+//     // correctly
+//     extractCSS: process.env.NODE_ENV === "production",
+//   },
+//   modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+//   content: {},
+//   colorMode: {
+//     classSuffix: "",
+//   },
+// };
